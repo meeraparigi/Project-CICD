b6cad813c3d047fc911852a2bb27d0bd2cd63582
@meeraparigi <meera22_99@yahoo.com>
2025-08-06 23:18:47 -0500

    Created terraform folder and moved the code to this folder

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+    type = string
+    description = "This is the default AWS Region used in this project"
+}
+
+variable "bucket_name" {
+    type = string
+    description = "This is the name of the S3 bucket"
+}
